02e3366eec551ded8a59b07908ff0f4888cf4de4
torriiia <vrkuznetsova@edu.hse.ru>
2025-04-30 11:14:43 +0300

    Update collect_files.sh

--- collect_files.sh
+++ collect_files.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-input_dir=$1
-output_dir=$2
+max_depth=""
+input_dir=""
+output_dir=""
+while:
+do
+    if [[ $# -eq 0 ]]; then
+        break
+    else
+      if [[ "$1" == "--max_depth" ]]; then
+        max_depth=$2
+        shift 2
+      elif [[ -z "input_dir" ]]; then
+        input_dir=$1
+        shift
+      else
+        output_dir=$2
+        shift
+      fi
+    fi
+done
 find "$input_dir" -type f -exec cp {} "$output_dir" \;
 exit 0
